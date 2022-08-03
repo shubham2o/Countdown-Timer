810d482a97675a10c9447444ae5985f5fbc74ced
shubham2o <shubhamsharma.ha@gmail.com>
2022-08-03 14:57:07 +0530

    HTML DOM Element title added

---
+++ Doubts/Doubts.js
@@ -0,0 +1,10 @@
+// HTML DOM Element title
+// EXAMPLE 1
+const element = document.getElementById('myAbbr');
+document.getElementById('demo').innerHTML = element.title;
+
+// EXAMPLE 2
+function myFunction() {
+    const element2 = document.getElementById("myH2");
+    element2.title = "The World's Largest Web Development Site";
+}
