41b6b3d727e0e6e277ac1270eba8f527679413d1
shubham2o <shubhamsharma.ha@gmail.com>
2022-08-03 15:05:01 +0530

    HTML DOM Document title added

--- Doubts/Doubts.js
+++ Doubts/Doubts.js
@@ -8,3 +8,11 @@ function myFunction() {
     const element2 = document.getElementById("myH2");
     element2.title = "The World's Largest Web Development Site";
 }
+
+// HTML DOM Document title
+// EXAMPLE 1
+document.getElementById("demo3").innerHTML = document.title;
+
+// EXAMPLE 2
+document.title = "A new title";
+document.getElementById("demo4").innerHTML = document.title;
